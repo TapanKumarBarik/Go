043c348b34502b5ba808f06745b896edb73ba1a8
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-09-17 18:03:12 +0530

    Intro

---
+++ 0002_lab/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myName string="Tapan"
+	var myAge int
+	myAge=24
+
+	//we can declare directly
+	myAddress:="No Address"
+
+	fmt.Println("Hello Go")
+	fmt.Println(myName)
+	fmt.Printf("%d \n",myAge)
+	fmt.Println(myAddress)
+}
