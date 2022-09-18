402f493ee0930824d92ed0f2a8cb23f3e38c3107
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-09-18 19:30:40 +0530

    Arrays

---
+++ 0007_Arrays/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	//initialize array	
+	var names [4]int
+	fmt.Println(names)
+	l:=len(names)
+	fmt.Println(l)
+
+	newArray:=[4]string{"Tapan","Kumar","Barik","Tapan kumar Barik"}
+	fmt.Println(newArray)
+
+}
